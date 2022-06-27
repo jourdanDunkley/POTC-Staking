56d807f62ebc92782b34e4ba8958a2311b587715
Jourdan <dunkley.jourdan@gmail.com>
2022-06-26 22:54:02 -0500

    preparing for tests

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -2,8 +2,14 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-
+import "@openzeppelin/utils/Strings.sol";
+import "../src/Staking/POTCStaking.sol";
+ 
 contract ContractTest is Test {
+
+    using stdStorage for StdStorage;
+    using Strings for uint256;
+    
     function setUp() public {}
 
     function testExample() public {
